e9af3cc58670977770742c319c7cb91ecabee3d5
cheikna.dansoko <cheikna.dansoko@etu.u-pec.fr>
2019-06-01 14:54:09 +0200

    Modification de l'insertion en base
    Maintenant, les positions ne sont plus insérées sur de nouvelles lignes mais elles sont mises à jour.

--- positions-inserter/positions-reader-inserter.py
+++ positions-inserter/positions-reader-inserter.py
@@ -2,11 +2,14 @@ import sqlite3
 import time
 
 databasePath = "position.sqlite"
-tableName = "ingAir"
+tableName = "ING_AIR"
 positionsFileName = "aircraft-positions.txt"
 # Creation du fichier s'il n'existe pas
 file = open(positionsFileName, 'w+')
 file.close()
+
+# ID qui va etre cree pour nos mises a jour de positions afin d'eviter d'avoir inserer de nouvelles lignes car sqlite est limite a 500 lignes
+myId = 0
 
 connection = sqlite3.connect(databasePath)
 connection.enable_load_extension(True)
@@ -14,38 +17,49 @@ connection.enable_load_extension(True)
 connection.execute("SELECT load_extension('mod_spatialite')")
 cursor = connection.cursor()
 
+print("Creation of the table " + tableName + " in progress. This operation may take some time (~3 minutes).")
 # Creation de la table
 cursor.executescript(""" 
 		DROP TABLE IF EXISTS  """ + tableName + """ ;
-		CREATE TABLE IF NOT EXISTS """ + tableName + """ (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, measured_value DOUBLE NOT NULL);
+		CREATE TABLE IF NOT EXISTS """ + tableName + """ (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, count INTEGER NOT NULL);
 		SELECT InitSpatialMetaData();
 		SELECT AddGeometryColumn('""" + tableName + """', 'the_geom', 4326, 'POINT', 'XY');
 	""")
 connection.commit()
 
-#Lecture des positions
+print("The table " + tableName + " has been created.")
+print("Processing positions...")
+#Lecture des positions et insertion dans la base de donnee
 f = open(positionsFileName, "r")
-f2 = open("database-log.txt", "a")
 previousLine = ''
 count = 1
 while(True):
 	currentLine = f.readline()
-	print(currentLine)
+	# Eviter d'inserer en base la meme donnee plusieurs fois de suite
 	if(currentLine == previousLine):
-		print("same as previousLine")
-		time.sleep(5)
+		time.sleep(3)
 	else :
-		print("new positions")
+		# Le regex de split est defini dans la classe Aircraft.cpp du projet positions-retriever fait en C++
 		positions = currentLine.split("and")
 		latitude = positions[0]
 		longitude = positions[1]
-		f2.write("latitude : " + latitude)
-		f2.write("longitude : " + longitude)
-		f2.write(str(count) + "=======================")
-		cursor.executescript("""
-			INSERT INTO """ + tableName + """ VALUES (NULL, 'execfile2 test """ + str(count) + """', 2.34567, GeomFromText('POINT(2.02 3.03)', 4326)); 
-			""")
+		point = longitude + " " + latitude
+		if(myId == 0):
+			# Insertion de la premiere position recuperee
+			cursor.executescript("""
+				INSERT INTO """ + tableName + """ VALUES (NULL, """ + str(count) + """, GeomFromText('POINT(""" + point + """)', 4326)); 
+				""")
+			# Recuperation de l'id correspondant a cette derniere ligne creee
+			cursor.execute(""" SELECT id FROM """ + tableName + """ ORDER BY id DESC LIMIT 1;""")
+			myId = cursor.fetchone()[0]
+			print("A new line has been created with the id : " + str(myId))
+
+		else :			
+			# Mise a jour des points ainsi que du count
+			query = 'UPDATE  '  + tableName + '  SET count =  ' + str(count) +' , the_geom = GeomFromText(\'POINT(' + point + ')\', 4326) WHERE id =  '+ str(myId) +';'
+			print(query)
+			cursor.execute(query)
 		connection.commit()
+	# Augmente afin de connaitre le nombre de tour de boucle qui a ete fait
 	count += 1
 f.close()
-f2.close()
